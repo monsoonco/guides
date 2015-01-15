b4f4c245232583aa19bcad6b48a3ed61712abf10
Gabe Berke-Williams <gabebw@gabebw.com>
2015-01-15 13:54:54 -0500

    Do not put timestamps at the bottom of a migration
    `rails generate` puts timestamps at the bottom of a migration by default,
    meaning we have to change every migration even if the migration is otherwise
    generated perfectly.
    
    Original discussion: https://github.com/thoughtbot/guides/pull/159

--- style/rails/migration.rb
+++ style/rails/migration.rb
@@ -1,9 +1,10 @@
 class CreateClearanceUsers < ActiveRecord::Migration
   def change
     create_table :users  do |t|
-      t.timestamps null: false
       t.string :email, null: false
       t.string :name, null: false, default: ''
+
+      t.timestamps null: false
     end
 
     add_index :users, :email
